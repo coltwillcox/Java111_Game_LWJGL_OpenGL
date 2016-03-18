8521c262bd338c9a312242c48cbbe08cc3248d99
colt <necromanga@gmail.com>
2016-03-18 14:35:07 +0100

    new

--- src/renderEngineTester/MainGameLoop.java
+++ src/renderEngineTester/MainGameLoop.java
@@ -5,6 +5,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 /**
  * Created by colt on 3/17/16.
@@ -16,6 +17,8 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+
         //OpenGL expects vertices to be defined counter clockwise by default.
         float[] vertices = {
                 -0.5f, 0.5f, 0f,    //V0
@@ -27,13 +30,20 @@ public class MainGameLoop {
                 0, 1, 3,    //Top left triangle (V0, V1, V3).
                 3, 1, 2     //Bottom right triangle (V3, V1, V2).
         };
+
         RawModel model = loader.loadToVAO(vertices, indices);
+
         //Game logic, render...
         while(!Display.isCloseRequested()){
             renderer.prepare();
+            shader.start();
             renderer.render(model);
+            shader.stop();
             DisplayManager.updateDisplay();
         }
+
+        //Clear memory and close program.
+        shader.cleanUo();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
