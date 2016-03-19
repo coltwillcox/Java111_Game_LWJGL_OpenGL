4833c1497b495e9e25b35483f70c51ef168cd6ed
colt <necromanga@gmail.com>
2016-03-19 12:17:58 +0100

    Loading 3D Models

--- src/renderEngineTester/MainGameLoop.java
+++ src/renderEngineTester/MainGameLoop.java
@@ -8,6 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -24,87 +25,16 @@ public class MainGameLoop {
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
 
-        //OpenGL expects vertices to be defined counter clockwise by default.
-        float[] vertices = {
-                -0.5f, 0.5f, 0,
-                -0.5f, -0.5f, 0,
-                0.5f, -0.5f, 0,
-                0.5f, 0.5f, 0,
 
-                -0.5f, 0.5f, 1,
-                -0.5f, -0.5f, 1,
-                0.5f, -0.5f, 1,
-                0.5f, 0.5f, 1,
 
-                0.5f, 0.5f, 0,
-                0.5f, -0.5f, 0,
-                0.5f, -0.5f, 1,
-                0.5f, 0.5f, 1,
-
-                -0.5f, 0.5f, 0,
-                -0.5f, -0.5f, 0,
-                -0.5f, -0.5f, 1,
-                -0.5f, 0.5f, 1,
-
-                -0.5f, 0.5f, 1,
-                -0.5f, 0.5f, 0,
-                0.5f, 0.5f, 0,
-                0.5f, 0.5f, 1,
-
-                -0.5f, -0.5f, 1,
-                -0.5f, -0.5f, 0,
-                0.5f, -0.5f, 0,
-                0.5f, -0.5f, 1
-        };
-        int[] indices = {
-                0, 1, 3,
-                3, 1, 2,
-                4, 5, 7,
-                7, 5, 6,
-                8, 9, 11,
-                11, 9, 10,
-                12, 13, 15,
-                15, 13, 14,
-                16, 17, 19,
-                19, 17, 18,
-                20, 21, 23,
-                23, 21, 22
-        };
-        float[] textureCoords = {
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1,
-                1, 0
-        };
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("textureJoy")));
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+        RawModel model = OBJLoader.loadObjModel("stall", loader);
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -10), 0, 0, 0, 1);
         Camera camera = new Camera();
 
         //Game logic, render...
         while (!Display.isCloseRequested()) {
-            entity.increaseRotation(1, 1, 1);
+            entity.increaseRotation(0, 1, 0);
             camera.move();
             renderer.prepare();
             shader.start();
